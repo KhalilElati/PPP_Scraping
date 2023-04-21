420474eb2c6cbef256252b0b00471b089d2a0795
Bzzzd <mohamedaziz.bouzid@insat.ucar.tn>
2023-04-21 09:39:50 +0100

    added comments lel wahch

--- tayara/tayara kol fel kol.py
+++ tayara/tayara kol fel kol.py
@@ -10,17 +10,17 @@ with open('tayara/test_tayara1.html','wb') as f:
     f.write(r.content)
 
 soup = BeautifulSoup(r.content, 'html.parser')
+
+#removing ad content from the page
 target= soup.find('h1',class_="text-2xl font-bold text-gray-700",string="Immobilier")
 for sibling in target.previous_siblings:
     sibling.extract()
+#finding item links
 search=soup.find_all(href=re.compile('item/.*'))
 links=[]
 for link in search:
     links.append(link.get('href'))
 del links[0]
-
-#print(links)
-
 
 data=[['type','superficie','Salles de bains','Chambres','Prix','Description']]  
 
@@ -32,10 +32,13 @@ for link in links:
     soup2 = BeautifulSoup(r2.content, 'html.parser')
     output1=soup2.find_all('span',class_='text-gray-700 text-xs font-medium')
     temp=[]
+    #extracting the type, superficie, salles de bains and chambres
     for element in output1:
         temp.append(element.text)
+    #exctracting the price
     prix=soup2.find('data',class_="text-red-600 font-bold font-arabic text-2xl")['value']
     temp.append(prix)
+    #extracting the description
     temp.append(soup2.find('h1',class_='text-gray-700 font-bold text-2xl font-arabic').text)
     data.append(temp)
     
