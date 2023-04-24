7dd3b12fb98f71bed207aa8a942e613313488a1e
Bzzzd <mohamedaziz.bouzid@insat.ucar.tn>
2023-04-24 14:48:42 +0100

    switch output from csv to json format

--- tayara/tayara kol fel kol.py
+++ tayara/tayara kol fel kol.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import csv
+import json
+
 
 url="https://www.tayara.tn/ads/c/Immobilier/"
 r=requests.get(url)
@@ -22,18 +24,39 @@ for link in search:
     links.append(link.get('href'))
 del links[0]
 
-data=[['type','superficie','Salles de bains','Chambres','Prix','Description']]  
+data=[]
 
 for link in links:
     url2="https://www.tayara.tn"+link
+    
     r2=requests.get(url2)
     with open('tayara/test_tayara2.html','wb') as f:
         f.write(r2.content)
     soup2 = BeautifulSoup(r2.content, 'html.parser')
     output1=soup2.find_all('span',class_='text-gray-700 text-xs font-medium')
-    temp=[]
+    if output1==[]:
+        continue
+    type=output1[0].text
+    superficie=output1[1].text
+    salles_de_bains=output1[2].text
+    chambres=output1[3].text
+    prix=soup2.find('data',class_="text-red-600 font-bold font-arabic text-2xl")['value']
+    description=soup2.find('h1',class_='text-gray-700 font-bold text-2xl font-arabic').text
+    temp={
+        'type':type,
+        'superficie' : superficie,
+        'Salles de bains':salles_de_bains,
+        'Chambres':chambres,
+        'Prix':prix ,
+        'Description': description
+    }
+    data.append(temp)
+    
+    
+    
+    
     #extracting the type, superficie, salles de bains and chambres
-    for element in output1:
+    """ for element in output1:
         temp.append(element.text)
     #exctracting the price
     prix=soup2.find('data',class_="text-red-600 font-bold font-arabic text-2xl")['value']
@@ -43,9 +66,12 @@ for link in links:
     data.append(temp)
     
 
-
-
 with open('tayara/output.csv', mode='w', newline='',encoding='utf-8') as file:
     writer = csv.writer(file)
     for row in data:
-        writer.writerow(row)
+        writer.writerow(row) """
+        
+        
+with open("tayara/data.json", "w",encoding="utf-8") as f:
+    # write JSON data to file
+    json.dump(data, f) 
